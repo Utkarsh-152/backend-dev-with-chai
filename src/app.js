0f625a9af856ef4de3257dc84ce1c80c1fed142d
Utkarsh Tripathi <shreytripathi2004@gmail.com>
2025-03-22 11:04:46 +0530

    user_controller auth

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express, { urlencoded } from 'express'
+import express from "express"
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-app = express()
+const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -12,4 +12,11 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export {app}
+// routes import
+import userRouter from './routes/user.route.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+export  {app}
